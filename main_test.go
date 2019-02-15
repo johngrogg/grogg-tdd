842fb7bab3b0f2556558c0256f82b5187f1ae245
John Grogg <john.grogg@wework.com>
2019-02-15 15:28:53 -0800

    T1 - First failing test for an abstraction layer

--- main_test.go
+++ main_test.go
@@ -1,19 +1,58 @@
 package main_test
 
 import (
+	"errors"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	. "github.com/johngrogg/grogg-tdd"
 )
 
+type invalidValidator struct{}
+
+func (invalidValidator) Validate(str string) error {
+	return errors.New("invalid")
+}
+
+type successValidator struct{}
+
+func (successValidator) Validate(str string) error {
+	return nil
+}
+
 var _ = Describe("Main", func() {
 	Describe("#RegisterSchema", func() {
+		var (
+			validator Validator
+
+			schema *Schema
+			err    error
+		)
+
+		BeforeEach(func() {
+			validator = successValidator{}
+		})
+
+		JustBeforeEach(func() {
+			schema, err = RegisterSchema("{}", validator)
+		})
+
 		It("should work", func() {
-			schema, err := RegisterSchema("{}")
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(schema.JSONSchema).Should(Equal("{}"))
 			Ω(schema.ID).ShouldNot(BeNil())
+		})
+
+		Context("when the schema string is invalid", func() {
+
+			BeforeEach(func() {
+				validator = invalidValidator{}
+			})
+
+			It("should return an error", func() {
+				Ω(err).Should(HaveOccurred())
+			})
 		})
 	})
 })
